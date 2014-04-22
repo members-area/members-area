30c5e155e844d709dd533e8f9217440f31ea36ac
Benjie Gillam <benjie@jemjie.com>
2014-04-22 21:11:48 +0100

    Even more ignores.

--- bootstrap.sh
+++ bootstrap.sh
@@ -73,7 +73,7 @@ ARGS="--watch ../members-area"
 for I in ../members-area-*; do
   ARGS="\$ARGS --watch \$I";
 done
-./node_modules/.bin/nodemon --ignore node_modules/ --ignore public/ --ignore db/ --ignore views/ \$ARGS --watch . index.coffee
+./node_modules/.bin/nodemon --ignore node_modules/ --ignore public/ --ignore db/ --ignore views/ --ignore app/views/ --ignore app/db/ --ignore scripts/ --ignore sessions/ --ignore log/ \$ARGS --watch . index.coffee
 EOF
 chmod +x watch.sh
 
